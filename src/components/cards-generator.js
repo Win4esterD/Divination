feec88551e79accd6f97d45465ba5b5aa1482d78
Pavel Didenko <win4ester2016@yandex.ru>
2023-02-26 08:45:39 +0300

    -feat: implamant cards' images

--- src/components/cards-generator.js
+++ src/components/cards-generator.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import WaiteIMGs from "../JSON/waite-images.json";
+import cardsRus from "../JSON/cards-rus.json";
 
 class CardsGenerator extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.revealCard = this.revealCard.bind(this);
+  }
+
 
   getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -9,9 +16,18 @@ class CardsGenerator extends React.Component{
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  //Testing cards
+  revealCard(event){
+    const pentagram = document.querySelector('.main-pentagram__img');
+    event.target.style.backgroundImage = `url(${WaiteIMGs[Number(event.target.id) - 1].image})`;
+    pentagram.src = WaiteIMGs[Number(event.target.id) - 1].image;
+    console.log(cardsRus[Number(event.target.id) - 1].name);
+  }
+
+  //onClick function - testing cards
   generateOneCard(num){
     return (
-      <div id={String(num)} key={String(num)} className="card"></div>
+      <div id={String(num)} key={String(num)} className="card" onClick={this.revealCard}></div>
     )
   }
 
@@ -20,7 +36,7 @@ class CardsGenerator extends React.Component{
     const numCounter = [];
 
     while(result.length !== 78){
-      let randInt = this.getRandomInt(0, 78);
+      let randInt = this.getRandomInt(1, 79);
       if(!numCounter.includes(randInt)){
         numCounter.push(randInt);
         result.push(this.generateOneCard(randInt))
