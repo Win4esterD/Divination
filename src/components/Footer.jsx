bd063c1204b15bebd5d0e52d21be2a6c39c2ed0a
Pavel Didenko <win4ester2016@yandex.ru>
2023-03-08 22:19:31 +0300

    feat: implement footer with the links to Github and LinkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,12 @@ class Footer extends React.Component{
   render(){
     return(
       <footer>
-
+        <div className="gh-wrapper">
+          <a href="https://github.com/Win4esterD"><img className="gh-img" src="./assets/IMG/github.png" alt="github"></img> <a className="gh-link" href="https://github.com/Win4esterD">Win4esterD</a></a>
+        </div>
+        <div className="ln-wrapper">
+          <a href="https://www.linkedin.com/in/pavel-didenko-9b6371181/"><img src="./assets/IMG/linkedIn.png" className="ln-image"></img></a>
+        </div>
       </footer>
     )
   }
