4b44e844a4eb3f353f097280fd5ba7c847df0402
Pavel Didenko <patriot2006_06@mail.ru>
2023-04-05 19:29:59 +0300

    feat: partially implement greetings menu

---
+++ src/components/greetings.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../styles/greetings-menu.scss';
+
+class GreetingsMenu extends React.Component{
+  setText(){
+    const russian = "Добро пожаловать в приложение для гадания на картах ТАРО. Это твой ритуальный стол для гадания. Выбери расклад в верхней части экрана, подумай о вопросе, который хочешь задать оракулу, после чего начни тянуть карты. Более подробно о раскладах можешь узнать в разделе “Теория”.";
+    return russian;
+  }
+
+  render(){
+    return(
+      <div className="greetings-menu">
+        <div className="greetings">
+          <p className="greetings-text">{this.setText()}</p>
+          <button className="ok-button">
+            <b className="ok">OK</b>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GreetingsMenu;
