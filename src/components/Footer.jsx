ea4ffd8d9237b16dfc717a17bc3356e740b193e9
Pavel Didenko <patriot2006_06@mail.ru>
2023-10-16 03:48:16 +0300

    refactor: Footer to functional component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import "../styles/footer.scss";
 
-class Footer extends React.Component{
-  render(){
-    return(
+const Footer = () => {
+    return (
       <footer>
         <div className="gh-wrapper">
-          <a href="https://github.com/Win4esterD"><img className="gh-img" src="./assets/IMG/github.png" alt="github"></img> <a className="gh-link" href="https://github.com/Win4esterD">Win4esterD</a></a>
+          <a href="https://github.com/Win4esterD">
+            <img
+              className="gh-img"
+              src="./assets/IMG/github.png"
+              alt="github"
+            ></img>
+          </a>
+          <a className="gh-link" href="https://github.com/Win4esterD">
+            Win4esterD
+          </a>
         </div>
         <div className="ln-wrapper">
-          <a href="https://www.linkedin.com/in/pavel-didenko-9b6371181/"><img src="./assets/IMG/linkedIn.png" className="ln-image"></img></a>
+          <a href="https://www.linkedin.com/in/pavel-didenko-9b6371181/">
+            <img src="./assets/IMG/linkedIn.png" className="ln-image"></img>
+          </a>
         </div>
       </footer>
-    )
-  }
+    );
 }
 
 export default Footer;
