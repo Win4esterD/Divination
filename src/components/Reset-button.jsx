b98cd8f0a7be1cdab4a3b8e461219e600af9aaac
Pavel Didenko <patriot2006_06@mail.ru>
2023-10-17 06:54:18 +0300

    refactor: Restart button to functional component

--- src/components/Reset-button.jsx
+++ src/components/Reset-button.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import '../styles/components/reset-button.scss';
 
-class ResetButton extends React.Component{
-  setButtonLanguage(){
-    const language = this.props.language;
-    if(this.props.language === "Русский"){
-      return "Начать Заново";
-    }else if(this.props.language === "English"){
-      return "Start Over";
-    }else if(this.props.language === "Español"){
-      return "De Nuevo";
-    }
-  }
-  render(){
-    return(
-      <button className="reset-button" onClick={this.props.resetButtonHandler}>{this.setButtonLanguage()}</button>
-    )
-  }
-}
+const buttonLanguages = {
+  Русский: "Начать Заново",
+  English: "Start Over",
+  Español: "De Nuevo",
+};
+
+const ResetButton = ({ resetButtonHandler, language }) => {
+  return (
+    <button className="reset-button" onClick={resetButtonHandler}>
+      {buttonLanguages[language]}
+    </button>
+  );
+};
 
 export default ResetButton;
